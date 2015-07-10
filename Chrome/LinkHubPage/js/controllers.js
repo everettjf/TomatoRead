31d10d944603bfe3c6fac4d41a7215b6027eb5a0
everettjf <everettjf@live.com>
2015-07-10 13:27:55 +0800

    chromes

---
+++ Chrome/LinkHubPage/js/controllers.js
@@ -0,0 +1,20 @@
+/**
+ * Created by everettjf on 15/7/10.
+ */
+'use strict';
+
+var linkControllers = angular.module('linkControllers',[]);
+
+
+linkControllers.controller('LinkSingleCtrl',['$scope','$http',function($scope,$http){
+
+}]);
+linkControllers.controller('LinkLoginCtrl',['$scope','$http',function($scope,$http){
+
+}]);
+linkControllers.controller('LinkTabCtrl',['$scope','$http',function($scope,$http){
+
+}]);
+linkControllers.controller('LinkImportCtrl',['$scope','$http',function($scope,$http){
+
+}]);
