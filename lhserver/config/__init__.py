b85f2a069d6e020a82ddc278a34f34ba6c039ac5
everettjf <everettjf@live.com>
2015-10-19 21:48:57 +0800

    Revert "flask-mongoengine"
    This reverts commit ade35e9a5b4dc5b0ea620fcb7ff297f2094e08f0.

--- lhserver/config/__init__.py
+++ lhserver/config/__init__.py
@@ -1,3 +1,2 @@
 DEBUG = True
 SECRET_KEY = 'd[\x0fI\x11O\xa0o\x84@vpp,\xbdOV@*\xd4\xf8\xb1}\x9d'
-MONGODB_DB = 'linkhub'
--- lhserver/linkhub.py
+++ lhserver/linkhub.py
@@ -1,37 +1,13 @@
 from flask import Flask,request,session,g,redirect,url_for,abort,render_template,flash
-from flask.ext.mongoengine import MongoEngine
-import datetime
+from mongoengine import connect,errors
+from model import model
 import hashlib
 
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-db = MongoEngine()
-
-
-class User(db.Document):
-    email = db.StringField(required=True, unique=True)
-    blog_id = db.StringField(required=True, max_length=100, unique=True)
-    password = db.StringField(required=True, max_length=100)
-
-
-class Post(db.Document):
-    meta = {'allow_inheritance': True}
-
-    author = db.ReferenceField(User)
-    title = db.StringField(required=True, max_length=100)
-    tags = db.ListField(db.StringField(max_length=50))
-    created_at = db.DateTimeField(default=datetime.datetime.now)
-
-
-class LinkPost(Post):
-    link_url = db.StringField(required=True, unique_with=['author', 'title'])
-    click_events = db.ListField(db.DateTimeField(default=datetime.datetime.now))
-
-
-class TextPost(Post):
-    content = db.StringField(required=True, max_length=100, unique_with=['author', 'title'])
+connect('linkhub')
 
 
 @app.route('/')
@@ -53,7 +29,7 @@ def user_index(blog_id):
 def register():
     error = None
     if request.method == 'POST':
-        user = User()
+        user = model.User()
         user.email = request.form['email']
         user.password = request.form['password']
 
@@ -62,9 +38,9 @@ def register():
             user.save()
             flash('Register succeed')
             return redirect(url_for('user_index', blog_id=user.blog_id))
-        except db.NotUniqueError, err:
+        except errors.NotUniqueError, err:
             error = 'Save error (Email existed) :' + err.message
-        except db.OperationError, err:
+        except errors.OperationError, err:
             error = 'Save error : ' + err.message
 
     return render_template('register.html', error=error)
