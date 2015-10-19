ade35e9a5b4dc5b0ea620fcb7ff297f2094e08f0
everettjf <everettjf@live.com>
2015-10-19 21:37:18 +0800

    flask-mongoengine

--- lhserver/linkhub.py
+++ lhserver/linkhub.py
@@ -1,13 +1,37 @@
 from flask import Flask,request,session,g,redirect,url_for,abort,render_template,flash
-from mongoengine import connect,errors
-from model import model
+from flask.ext.mongoengine import MongoEngine
+import datetime
 import hashlib
 
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-connect('linkhub')
+db = MongoEngine()
+
+
+class User(db.Document):
+    email = db.StringField(required=True, unique=True)
+    blog_id = db.StringField(required=True, max_length=100, unique=True)
+    password = db.StringField(required=True, max_length=100)
+
+
+class Post(db.Document):
+    meta = {'allow_inheritance': True}
+
+    author = db.ReferenceField(User)
+    title = db.StringField(required=True, max_length=100)
+    tags = db.ListField(db.StringField(max_length=50))
+    created_at = db.DateTimeField(default=datetime.datetime.now)
+
+
+class LinkPost(Post):
+    link_url = db.StringField(required=True, unique_with=['author', 'title'])
+    click_events = db.ListField(db.DateTimeField(default=datetime.datetime.now))
+
+
+class TextPost(Post):
+    content = db.StringField(required=True, max_length=100, unique_with=['author', 'title'])
 
 
 @app.route('/')
@@ -29,7 +53,7 @@ def user_index(blog_id):
 def register():
     error = None
     if request.method == 'POST':
-        user = model.User()
+        user = User()
         user.email = request.form['email']
         user.password = request.form['password']
 
@@ -38,9 +62,9 @@ def register():
             user.save()
             flash('Register succeed')
             return redirect(url_for('user_index', blog_id=user.blog_id))
-        except errors.NotUniqueError, err:
+        except db.NotUniqueError, err:
             error = 'Save error (Email existed) :' + err.message
-        except errors.OperationError, err:
+        except db.OperationError, err:
             error = 'Save error : ' + err.message
 
     return render_template('register.html', error=error)
