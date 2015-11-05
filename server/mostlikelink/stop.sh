419f0cd816f8c64a82f6924d1d0021e04fffb1d6
everettjf <everettjf@live.com>
2015-11-05 20:36:45 +0800

    run

---
+++ server/mostlikelink/stop.sh
@@ -0,0 +1,1 @@
+kill `cat mostlikelink.pid`
--- server/run.py
+++ server/run.py
@@ -7,4 +7,7 @@ from mostlikelink.userapis import *
 import os
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    if os.environ.get('MOSTLIKELINK_PRODUCTION') is not None:
+        app.run(host='0.0.0.0', port=80)
+    else:
+        app.run(host='0.0.0.0', port=5000)
