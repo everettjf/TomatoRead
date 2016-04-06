c2f32a9f0bea4b3c18149a72caf6a1f1717d0b35
everettjf <everettjf@live.com>
2016-04-07 01:42:33 +0800

    order and feed

--- web/x123/restapi.py
+++ web/x123/restapi.py
@@ -7,7 +7,7 @@ from .utils import JSONResponse, parse_request_json
 from django.views.decorators.http import require_GET, require_POST
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
-from django.db.models import F
+from django.db.models import F, Q
 
 
 class DomainList(generics.ListAPIView):
@@ -26,7 +26,7 @@ class AngleList(generics.ListAPIView):
 
 
 class BookmarkList(generics.ListAPIView):
-    queryset = Bookmark.objects.all()
+    queryset = Bookmark.objects.all().order_by('-created_at')
     serializer_class = BookmarkSerializer
 
 
@@ -36,7 +36,14 @@ class BookmarkListInAspect(generics.ListAPIView):
     def get_queryset(self):
         # print('query params : ')
         # print(self.kwargs)
-        return Bookmark.objects.filter(aspect_id=self.kwargs['aspect_id'])
+        return Bookmark.objects.filter(aspect_id=self.kwargs['aspect_id']).order_by('-created_at')
+
+
+class FeedList(generics.ListAPIView):
+    serializer_class = BookmarkSerializer
+
+    def get_queryset(self):
+        return Bookmark.objects.filter(~Q(feed_url='')).order_by('-created_at')
 
 
 class BookmarkDetail(generics.RetrieveAPIView):
--- web/x123/urls.py
+++ web/x123/urls.py
@@ -20,12 +20,13 @@ urlpatterns = [
     url(r'^api/bookmarks/$', restapi.BookmarkList.as_view()),
     url(r'^api/bookmarks/(?P<pk>[0-9]+)/$', restapi.BookmarkDetail.as_view()),
 
-    url(r'^api/bookmarks_in_aspect/(?P<aspect_id>[0-9]+)/$', restapi.BookmarkListInAspect.as_view()),
-
     url(r'^api/auth_info/$', restapi.auth_info),
     url(r'^api/bookmark_existed/$', restapi.bookmark_existed),
     url(r'^api/bookmark_save/$', restapi.bookmark_save),
     url(r'^api/bookmark_remove/$', restapi.bookmark_remove),
     url(r'^api/bookmark_click/$', restapi.bookmark_click),
+
+    url(r'^api/bookmarks_in_aspect/(?P<aspect_id>[0-9]+)/$', restapi.BookmarkListInAspect.as_view()),
+    url(r'^api/feeds/$', restapi.FeedList.as_view()),
 ]
 
