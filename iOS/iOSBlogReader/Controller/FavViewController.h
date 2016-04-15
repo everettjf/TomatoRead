4df17111688cf16774f664062a1e969804b3d97f
everettjf <everettjf@live.com>
2016-04-16 02:09:26 +0800

    fav

---
+++ iOS/iOSBlogReader/Controller/FavViewController.h
@@ -0,0 +1,13 @@
+//
+//  FavViewController.h
+//  iOSBlogReader
+//
+//  Created by everettjf on 16/4/16.
+//  Copyright © 2016年 everettjf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavViewController : UIViewController
+
+@end
