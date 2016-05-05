72a24ddb3254bb99c57604cf639d1e35cf848802
everettjf <everettjf@live.com>
2016-05-06 01:36:59 +0800

    ok

--- jianspider/jianspider/spiders/domz_spiders.py
+++ jianspider/jianspider/spiders/domz_spiders.py
@@ -1,14 +1,42 @@
 import scrapy
+from jianspider.items import JianspiderItem
 
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ["dmoz.org"]
     start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+        # "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        # "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/",
     ]
 
+    # def parse(self, response):
+    #     # filename = response.url.split("/")[-2] + '.html'
+    #     # with open(filename, 'wb') as f:
+    #     #     f.write(response.body)
+    #
+    #     # for sel in response.xpath('//ul/li'):
+    #     #     title = sel.xpath('a/text()').extract()
+    #     #     link = sel.xpath('a/@href').extract()
+    #     #     desc = sel.xpath('text()').extract()
+    #     #     print title, link, desc
+    #
+    #     for sel in response.xpath('//ul/li'):
+    #         item = JianspiderItem()
+    #         item['title'] = sel.xpath('a/text()').extract()
+    #         item['link'] = sel.xpath('a/@href').extract()
+    #         item['desc'] = sel.xpath('text()').extract()
+    #         yield item
+
     def parse(self, response):
-        filename = response.url.split("/")[-2] + '.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        for href in response.css("ul.directory.dir-col > li > a::attr('href')"):
+            url = response.urljoin(href.extract())
+            yield scrapy.Request(url, callback=self.parse_dir_contents)
+
+    def parse_dir_contents(self, response):
+        for sel in response.xpath('//ul/li'):
+            item = JianspiderItem()
+            item['title'] = sel.xpath('a/text()').extract()
+            item['link'] = sel.xpath('a/@href').extract()
+            item['desc'] = sel.xpath('text()').extract()
+            yield item
--- web/export.py
+++ web/export.py
@@ -1,11 +1,18 @@
+#-*- coding:utf-8 -*-
+
 import os
 import django
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'iosblog.settings'
 django.setup()
 
-from x123.models import Domain
+# from .x123.models import Domain, Bookmark, Aspect
+from x123.models import Domain, Bookmark, Aspect
+
 import json
+from django.db.models import F, Q
+from django.core.paginator import Paginator
+import hashlib
 
 target_json_dir = '/Users/everettjf/GitHub/everettjf.github.com/app/blogreader/'
 target_markdown_dir = '/Users/everettjf/GitHub/iOSBlog/'
@@ -13,22 +20,94 @@ target_markdown_dir = '/Users/everettjf/GitHub/iOSBlog/'
 ########################################################
 
 
+# Util
+def local_open(filename):
+    return open(os.path.join(target_json_dir, filename), 'w+', encoding='utf-8')
+
+
+def local_dump(obj, f):
+    json.dump(obj, f, indent=4, ensure_ascii=False)
+
+
 # Json export
 def export_json():
-    # Domain
-    with open(os.path.join(target_json_dir, 'domain.json'), 'w+') as f:
-        res_domains = []
+    # Domains
+    with local_open('domains.json') as f:
+        domains = []
         for domain in Domain.objects.all():
-            res_domains.append({
+            aspects = []
+            for aspect in domain.aspect_set.all():
+                aspects.append({
+                    'id': aspect.id,
+                    'name': aspect.name,
+                    'zindex': aspect.zindex,
+                })
+
+            domains.append({
                 'id': domain.id,
-                'name': domain.name
+                'name': domain.name,
+                'zindex': domain.zindex,
+                'aspects': aspects,
             })
-        json.dump(res_domains, f)
+        local_dump(domains,f)
+    print('domains exported')
 
-    # Aspect
-    # with open
+    # Feeds
+    for domain in Domain.objects.all():
+        jsonfilename = '%d_feeds.json' % domain.id
 
+        version = hashlib.md5()
+        with local_open(jsonfilename) as f:
+            feeds = []
+            for link in Bookmark.objects.filter(~Q(feed_url='')).order_by('-created_at', '-zindex'):
+                feeds.append({
+                    'id': link.id,
+                    'name': link.name,
+                    'url': link.url,
+                    'favicon': link.favicon,
+                    'feed_url': link.feed_url,
+                    'zindex': link.zindex,
+                    'created_at': link.created_at.timestamp(),
+                })
 
+                version.update(link.feed_url.encode('utf-8'))
+            local_dump(feeds,f)
+
+        versionfilename = '%d_feeds.version' % domain.id
+        with local_open(versionfilename) as f:
+            f.write(version.hexdigest())
+
+        print(jsonfilename, 'exported , version=',version.hexdigest())
+
+    # Links
+    for aspect in Aspect.objects.all():
+        linkmodels = Bookmark.objects.filter(aspect_id=aspect.id).order_by('-created_at')
+        p = Paginator(linkmodels,20)
+        p.count
+        p.num_pages
+        for num in p.page_range:
+            pagemodels = p.page(num).object_list
+
+            filename = '%d_%d_link%d.json' % (aspect.domain_id, aspect.id, num)
+            with local_open(filename) as f:
+                pagelinks = []
+                for link in pagemodels:
+                    pagelinks.append({
+                        'id': link.id,
+                        'name': link.name,
+                        'url': link.url,
+                        'favicon': link.favicon,
+                        'feed_url': link.feed_url,
+                        'zindex': link.zindex,
+                        'created_at': link.created_at.timestamp(),
+                    })
+                local_dump({
+                    'count': p.count,
+                    'num_pages': p.num_pages,
+                    'cur_page': num,
+                    'links' : pagelinks
+                }, f)
+            print(filename, ' exported')
 
 ########################################################
 if __name__ == '__main__':
