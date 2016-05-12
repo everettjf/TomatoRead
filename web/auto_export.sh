b16e0ffd866de53825f76fa125389984da0311a0
everettjf <everettjf@live.com>
2016-05-12 16:12:07 +0000

    autocommit2016-05-12T16:12:07.138115

--- web/auto_export.sh
+++ web/auto_export.sh
@@ -4,27 +4,27 @@ cd /root/iOSBlog/
 echo 'pull'
 git pull
 echo 'local3.5'
-pyenv local 3.5.0
+pyenv local 3.5.0 >> /root/log.txt
 
 cd web
 echo 'export'
-python export.py
+python export.py >> /root/log.txt
 cd ..
 
 
 echo 'local'
-pyenv local 2.7.10
+pyenv local 2.7.10 >> /root/log.txt
 cd jianspider
 
 echo 'crawl'
-scrapy crawl jianshu
+scrapy crawl jianshu >> /root/log.txt
 
 cd ..
 
 
 cd web
 echo 'local'
-pyenv local 3.5.0
+pyenv local 3.5.0 >> /root/log.txt
 
 echo 'git commit'
 python gitcommit.py
