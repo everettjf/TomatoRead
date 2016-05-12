6d56f19c4567287d3dd8fbbd785b1be93e05768e
everettjf <everettjf@live.com>
2016-05-12 16:18:07 +0000

    autocommit2016-05-12T16:18:07.116806

--- web/auto_export.sh
+++ web/auto_export.sh
@@ -5,15 +5,18 @@ echo 'pull'
 git pull
 echo 'local3.5'
 pyenv local 3.5.0 >> /root/log.txt
+pyenv versions >> /root/log.txt
 
 cd web
 echo 'export'
 python export.py >> /root/log.txt
+python -V >> /root/log.txt
 cd ..
 
 
 echo 'local'
 pyenv local 2.7.10 >> /root/log.txt
+pyenv versions >> /root/log.txt
 cd jianspider
 
 echo 'crawl'
@@ -25,6 +28,7 @@ cd ..
 cd web
 echo 'local'
 pyenv local 3.5.0 >> /root/log.txt
+pyenv versions >> /root/log.txt
 
 echo 'git commit'
 python gitcommit.py
